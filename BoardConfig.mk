455695a18af2fed76904afde30418c1ca57c109f
Rizky <gameorrb9@gmail.com>
2026-06-16 09:55:28 +0000

    X1: move property to public

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,2 +1,3 @@
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += vendor/prize/camera/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += vendor/prize/camera/sepolicy/public
 VENDOR_SEPOLICY_DIRS  += vendor/prize/camera/sepolicy/vendor
